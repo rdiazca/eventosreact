31e9686e5a4d84a85b5d42e47d923a6bbfa03bfc
rdiazca <rdiazca96@gmail.com>
2022-03-20 01:11:44 -0400

    Leer los datos del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,9 +4,15 @@ import  { CategoriasConsumer } from '../context/CategoriasContext';
 class Formulario extends Component {
     state = { 
         nombre : '',
-        categoria : ''
-         
+        categoria : '' 
      } 
+
+     // Si el usuario agrega un evento o categoría
+     obtenerDatosEvento = e => {
+         this.setState({
+             [e.target.name] : e.target.value
+         })
+     }
     render() { 
         return (
             <form>
@@ -23,13 +29,16 @@ class Formulario extends Component {
                             className='uk-input'
                             type = 'text'
                             placeholder = 'Nombre de evento o ciudad'
+                            onChange={this.obtenerDatosEvento}
                         />
                     </div>
                     <div className='uk-margin' uk-margin = 'true'>
                         <select
                             className='uk-select'
                             name = 'categoria'
+                            onChange={this.obtenerDatosEvento}
                         >
+                            <option value=''> --Seleccione una categoría--</option>
                             <CategoriasConsumer>
                                 {(value) => {
                                    return(
